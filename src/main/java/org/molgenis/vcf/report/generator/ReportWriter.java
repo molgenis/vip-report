35d28343bbb9510a3ce02c9d09f2d2a9d2b015ad
Dennis Hendriksen <d.hendriksen@umcg.nl>
2020-05-19 10:27:10 +0200

    fix: default template resolving using jar

--- src/main/java/org/molgenis/vcf/report/generator/ReportWriter.java
+++ src/main/java/org/molgenis/vcf/report/generator/ReportWriter.java
@@ -6,15 +6,19 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import org.molgenis.vcf.report.model.Report;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
-import org.springframework.util.ResourceUtils;
 
 @Component
 public class ReportWriter {
+  private static final Logger LOGGER = LoggerFactory.getLogger(ReportWriter.class);
 
   public void write(Report report, Path outputFile, ReportWriterSettings reportWriterSettings) {
     try {
@@ -24,19 +28,25 @@ public class ReportWriter {
     }
   }
 
-  private void writeCheckedException(Report report, Path outputFile,
-      ReportWriterSettings reportWriterSettings)
+  private void writeCheckedException(
+      Report report, Path outputFile, ReportWriterSettings reportWriterSettings)
       throws IOException {
     if (reportWriterSettings.isOverwriteOutputReport()) {
       Files.deleteIfExists(outputFile);
     }
 
+    String templateHtml;
     Path templatePath = reportWriterSettings.getTemplatePath();
-    if (templatePath == null) {
-      templatePath = ResourceUtils.getFile("classpath:template-default.html").toPath();
+    if (templatePath != null) {
+      LOGGER.info("creating report using template {}", templatePath);
+      templateHtml = Files.readString(templatePath, UTF_8);
+    } else {
+      LOGGER.info("creating report using default template");
+      try (InputStream inputStream =
+          new ClassPathResource("template-default.html").getInputStream()) {
+        templateHtml = new String(inputStream.readAllBytes(), UTF_8);
+      }
     }
-
-    String templateHtml = Files.readString(templatePath, UTF_8);
     String scriptTag = createScriptTag(report, reportWriterSettings.isPrettyPrint());
     String reportHtml = templateHtml.replace("</head>", scriptTag + "</head>");
 
