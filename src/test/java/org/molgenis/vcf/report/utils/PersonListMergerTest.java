4660ca7404f088a6d9274f68d1612e21e8b6b489
bartcharbon <bart.charbon@gmail.com>
2020-06-03 08:00:32 +0200

    Fix unit test imports

--- src/test/java/org/molgenis/vcf/report/utils/PersonListMergerTest.java
+++ src/test/java/org/molgenis/vcf/report/utils/PersonListMergerTest.java
@@ -1,22 +1,18 @@
 package org.molgenis.vcf.report.utils;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import javafx.scene.control.ChoiceBox;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.molgenis.vcf.report.mapper.HtsJdkToRecordMapper;
 import org.molgenis.vcf.report.model.Items;
-import org.molgenis.vcf.report.utils.PedIndividual.AffectionStatus;
 import org.phenopackets.schema.v1.core.Pedigree.Person;
 import org.phenopackets.schema.v1.core.Pedigree.Person.AffectedStatus;
 import org.phenopackets.schema.v1.core.Sex;
