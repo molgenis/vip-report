f3f7f2780accdf0c5835342d399745328b76b9d3
bartcharbon <bart.charbon@gmail.com>
2022-04-05 13:23:52 +0200

    Fix 102: Genes file is incorrectly filtered for GRCh38 and other 'chr' prefixed builds

--- src/test/java/org/molgenis/vcf/report/genes/GenesFilterTest.java
+++ src/test/java/org/molgenis/vcf/report/genes/GenesFilterTest.java
@@ -48,6 +48,21 @@ class GenesFilterTest {
         decompress(genesFilter.filter(variants, 250)));
   }
 
+  @Test
+  void filterPrefixedBuild() {
+    VariantContext variantContext = mock(VariantContext.class);
+    Iterable<VariantContext> variants = singletonList(variantContext);
+    ContigInterval contigInterval = mock(ContigInterval.class);
+    when(contigInterval.getContig()).thenReturn("chr1");
+    when(contigInterval.getStart()).thenReturn(661200);
+    when(contigInterval.getStop()).thenReturn(661700);
+    when(vcfIntervalCalculator.calculate(variants, 250)).thenReturn(singletonList(contigInterval));
+
+    assertEquals(
+        "590\tNR_028327.1\tchr1\t-\t661138\t665731\t665731\t665731\t3\t661138,665277,665562,\t665184,665335,665731,\t0\tLOC100133331\tnone\tnone\t-1,-1,-1,\n",
+        decompress(genesFilter.filter(variants, 250)));
+  }
+
   private static String decompress(byte[] bytes) {
     try (GZIPInputStream gzipInputStream = new GZIPInputStream(new ByteArrayInputStream(bytes))) {
       return new String(gzipInputStream.readAllBytes(), StandardCharsets.UTF_8);
