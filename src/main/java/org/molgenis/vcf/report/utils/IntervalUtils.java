d15102570d5da7be8e015045099ca5da672cf8b5
bart.charbon <b.charbon@umcg.nl>
2023-11-17 20:21:26 +0100

    Improve interval merging

--- src/main/java/org/molgenis/vcf/report/utils/IntervalUtils.java
+++ src/main/java/org/molgenis/vcf/report/utils/IntervalUtils.java
@@ -17,8 +17,11 @@ public class IntervalUtils {
             for (int i = 1; i < intervals.size(); ++i) {
                 ContigInterval nextInterval = intervals.get(i);
                 if (nextInterval.getStart() <= interval.getStop() + 1) {
+                    //when intervals are based on crams the length can differ and although the start is greater
+                    //the stop position can be lesser than the previous interval
+                    int stop = interval.getStop() < nextInterval.getStop() ? nextInterval.getStop() : interval.getStop();
                     interval =
-                            new ContigInterval(interval.getContig(), interval.getStart(), nextInterval.getStop());
+                            new ContigInterval(interval.getContig(), interval.getStart(), stop);
                 } else {
                     mergedIntervals.add(interval);
                     interval = nextInterval;
