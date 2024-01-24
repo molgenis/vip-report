12ecb6236b3d2d5f0cff7180dd2252e20e8c226a
JoukjeKloosterman <j.kloosterman01@umcg.nl>
2024-01-24 09:13:44 +0100

    Feat: more streamed filtering less byte conversion

--- src/main/java/org/molgenis/vcf/report/bedmethyl/BedmethylFilter.java
+++ src/main/java/org/molgenis/vcf/report/bedmethyl/BedmethylFilter.java
@@ -1,6 +1,7 @@
 package org.molgenis.vcf.report.bedmethyl;
 
 
+import com.opencsv.CSVWriter;
 import com.opencsv.ICSVWriter;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
@@ -16,11 +17,10 @@ import org.molgenis.vcf.report.generator.SampleSettings;
 import org.molgenis.vcf.report.model.BedmethylLine;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-
 import static java.util.Objects.requireNonNull;
 
 public class BedmethylFilter {
@@ -42,47 +42,19 @@ public class BedmethylFilter {
 
     private byte[] filter(List<ContigInterval> contigIntervals) {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        List<BedmethylLine> bedmethylLines = Collections.emptyList();
-
-
-        if (bedmethylFile != null){
-            bedmethylLines = readBedmethylFile(bedmethylFile);
-        }
 
         try (
-                StringWriter stringWriter = new StringWriter()
-                ) {
-            StatefulBeanToCsv<BedmethylLine> beanToCsv = new StatefulBeanToCsvBuilder<BedmethylLine>(stringWriter)
-                    .withSeparator('\t')
-                    .withQuotechar(ICSVWriter.NO_QUOTE_CHARACTER)
+                InputStream fileStream = new FileInputStream(bedmethylFile.toFile());
+                Reader reader = new BufferedReader(new InputStreamReader(fileStream));
+                OutputStreamWriter streamWriter = new OutputStreamWriter(outputStream, StandardCharsets.UTF_8);
+                CSVWriter csvWriter = new CSVWriter(streamWriter,
+                        '\t', ICSVWriter.NO_QUOTE_CHARACTER,
+                        ICSVWriter.DEFAULT_ESCAPE_CHARACTER,
+                        ICSVWriter.DEFAULT_LINE_END)
+        ) {
+            StatefulBeanToCsv<BedmethylLine> beanToCsv = new StatefulBeanToCsvBuilder<BedmethylLine>(csvWriter)
                     .build();
 
-                for (BedmethylLine bedmethylLine : bedmethylLines) {
-                    boolean isAdded = false;
-                    for (ContigInterval contigInterval : contigIntervals) {
-                        if (!isAdded
-                                && bedmethylLine.getContig().equals(contigInterval.getContig())
-                                && isOverlappingFeature(bedmethylLine, contigInterval)) {
-
-                            beanToCsv.write(bedmethylLine);
-                            isAdded = true;
-                        }
-                    }
-                }
-            outputStream.write(stringWriter.toString().getBytes());
-
-        } catch (CsvRequiredFieldEmptyException | CsvDataTypeMismatchException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new  UncheckedIOException(e);
-        }
-
-        return outputStream.toByteArray();
-    }
-    private static List<BedmethylLine> readBedmethylFile(Path bedmethylFile) {
-        List<BedmethylLine> bedmethylLines;
-        try (InputStream fileStream = new FileInputStream(bedmethylFile.toFile());
-             Reader reader = new BufferedReader(new InputStreamReader(fileStream))) {
             CsvToBean<BedmethylLine> csvToBean =
                     new CsvToBeanBuilder<BedmethylLine>(reader)
                             .withSeparator('\t')
@@ -90,13 +62,26 @@ public class BedmethylFilter {
                             .withThrowExceptions(false)
                             .withIgnoreQuotations(true)
                             .build();
-            bedmethylLines = csvToBean.parse();
+            beanToCsv.write(csvToBean.stream().filter(bedmethylLine -> isFeature(contigIntervals, bedmethylLine)));
+        } catch (CsvRequiredFieldEmptyException | CsvDataTypeMismatchException e) {
+            throw new RuntimeException(e);
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
-        return bedmethylLines;
+        return outputStream.toByteArray();
     }
-    private boolean isOverlappingFeature(BedmethylLine bedmethylLine, ContigInterval contigInterval) {
+
+    private static boolean isFeature(List<ContigInterval> contigIntervals, BedmethylLine bedmethylLine) {
+        for (ContigInterval contigInterval : contigIntervals) {
+            if (bedmethylLine.getContig().equals(contigInterval.getContig())
+                    && isOverlappingFeature(bedmethylLine, contigInterval)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isOverlappingFeature(BedmethylLine bedmethylLine, ContigInterval contigInterval) {
         return (bedmethylLine.getStart() >= contigInterval.getStart()
                 && bedmethylLine.getStart() <= contigInterval.getStop()) // feature start in region
                 || (bedmethylLine.getEnd() >= contigInterval.getStart()
@@ -104,7 +89,5 @@ public class BedmethylFilter {
                 || (bedmethylLine.getStart() <= contigInterval.getStart()
                 && bedmethylLine.getEnd() >= contigInterval.getStop());
     }
-
-
 
 }
