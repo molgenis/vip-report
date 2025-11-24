0d81d3222d880095e5ed960fed211cef655f8260
bart.charbon <b.charbon@umcg.nl>
2025-11-24 10:39:13 +0100

    Fix db connection path

--- src/main/java/org/molgenis/vcf/report/utils/PathUtils.java
+++ src/main/java/org/molgenis/vcf/report/utils/PathUtils.java
@@ -16,18 +16,22 @@ public class PathUtils {
     return result;
   }
 
-    public static String getNameWithoutVcfExtensions(Path path) {
-        String filename = path.getFileName().toString();
-        if (filename.endsWith(".vcf.gz")) {
-            return filename.substring(0, filename.length() - 7);
-        } else if (filename.endsWith(".vcf")) {
-            return filename.substring(0, filename.length() - 4);
-        }
-        return filename;
+  public static String getNameWithoutVcfExtensions(Path path) {
+    String filename = path.getFileName().toString();
+    if (filename.endsWith(".vcf.gz")) {
+      return filename.substring(0, filename.length() - 7);
+    } else if (filename.endsWith(".vcf")) {
+      return filename.substring(0, filename.length() - 4);
     }
+    return filename;
+  }
 
-    public static String getDatabaseLocation(Path inputPath) {
-        Path parentDir = inputPath.getParent();
-        return String.format("%s.db", Path.of(String.valueOf(parentDir), getNameWithoutVcfExtensions(inputPath)));
-    }
+  public static String getDatabaseLocation(Path inputPath) {
+    Path parentDir = inputPath.getParent();
+    Path path =
+        parentDir != null
+            ? Path.of(String.valueOf(parentDir), getNameWithoutVcfExtensions(inputPath))
+            : Path.of(getNameWithoutVcfExtensions(inputPath));
+    return String.format("%s.db", path);
+  }
 }
