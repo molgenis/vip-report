a0b2f384a0d3cdd7d45c8580f12c635e4d535a62
dennishendriksen <d.hendriksen@umcg.nl>
2026-03-16 10:45:56 +0100

    build:more compiler linting

--- src/main/java/org/molgenis/vcf/report/generator/InvalidHpoLineException.java
+++ src/main/java/org/molgenis/vcf/report/generator/InvalidHpoLineException.java
@@ -2,7 +2,11 @@ package org.molgenis.vcf.report.generator;
 
 import static java.lang.String.format;
 
+import java.io.Serial;
+
 public class InvalidHpoLineException extends RuntimeException {
+  @Serial private static final long serialVersionUID = 1L;
+
   public InvalidHpoLineException(String line, String filename) {
     super(
         format(
--- src/main/java/org/molgenis/vcf/report/utils/UnexpectedCategoryException.java
+++ src/main/java/org/molgenis/vcf/report/utils/UnexpectedCategoryException.java
@@ -2,7 +2,11 @@ package org.molgenis.vcf.report.utils;
 
 import static java.lang.String.format;
 
+import java.io.Serial;
+
 public class UnexpectedCategoryException extends RuntimeException {
+  @Serial private static final long serialVersionUID = 1L;
+
   public UnexpectedCategoryException(String field, String stringValue) {
     super(format("Unexpected value '%s' for categorical field '%s'", stringValue, field));
   }
